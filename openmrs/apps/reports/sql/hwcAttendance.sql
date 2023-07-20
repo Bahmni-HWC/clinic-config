cd4c6dd762871c554beaadcf7a11f16d63a087ca
deepti.rawat <deepti.rawat@thoughtworks.com>
2023-07-20 12:30:00 +0530

    Refactor. Attendance Report
    Co-authored-by:yenugukeerthana <keerthana.yenugu@thoughtworks.com>

--- openmrs/apps/reports/sql/hwcAttendance.sql
+++ openmrs/apps/reports/sql/hwcAttendance.sql
@@ -5,6 +5,7 @@ DATE(encounter_date_time) AS "Attendance Date",
 "Attendance Time"
 from gok.center_attendance_form gaf
 JOIN users u ON gaf.created_by_id = u.id
-JOIN gok.address address ON gaf.address_id = address.id;
+JOIN gok.address address ON gaf.address_id = address.id
+where cast(DATE(encounter_date_time) as date) BETWEEN '#startDate#' AND '#endDate#'; 
 
 
