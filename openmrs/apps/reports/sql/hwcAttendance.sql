090faab90ba08140c46e422b7c5daaf0681dc6eb
deepti.rawat <deepti.rawat@thoughtworks.com>
2023-07-21 10:55:39 +0530

    Add. center name in prescription count report
    Co-authored-by:yenugukeerthana <keerthana.yenugu@thoughtworks.com>

--- openmrs/apps/reports/sql/hwcAttendance.sql
+++ openmrs/apps/reports/sql/hwcAttendance.sql
@@ -1,11 +1,11 @@
 select
-u.name AS "Name",
-address."City/Village" AS "Location",
+u.name AS "User Name",
+center_name.first_name AS "Center Name",
 DATE(encounter_date_time) AS "Attendance Date",
 "Attendance Time"
 from gok.center_attendance_form gaf
 JOIN users u ON gaf.created_by_id = u.id
-JOIN gok.address address ON gaf.address_id = address.id
+JOIN gok.center center_name ON gaf.individual_id = center_name.id
 where cast(DATE(encounter_date_time) as date) BETWEEN '#startDate#' AND '#endDate#'; 
 
 
