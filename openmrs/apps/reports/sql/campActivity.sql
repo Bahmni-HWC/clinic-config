247ff728f0db80f2ef71c6b6f9892268b4892278
riyaTw <126869458+riyaTw@users.noreply.github.com>
2023-08-10 16:12:49 +0530

    GOK-257 | Refactor. Camp activity and HWC attendence query to not use etl tables (#79)

--- openmrs/apps/reports/sql/campActivity.sql
+++ openmrs/apps/reports/sql/campActivity.sql
@@ -1,12 +1,15 @@
-select 
-first_name AS "Activity Name",
-"Activity Type",
-"Activity Date",
-"Activity Description",
-concat(address."State",', ',address."District",', ',address."Sub District",', ',address."City/Village") AS "Address",
-u.name AS "Added By",
-registration_location AS "GPS Coordinates" 
-from gok.camp_activity ca
-join gok.address address on ca.address_id = address.id
-join public.users u on ca.created_by_id = u.id
-where cast("Activity Date" as date) BETWEEN '#startDate#' AND '#endDate#'; 
+SELECT
+	ind.first_name AS "Activity Name",
+    c1.name AS "Activity Type",
+    CAST(ind.observations ->> '2d769525-e4b4-4ba7-9253-9d071b83f115' AS TIMESTAMP) AS "Activity Date",
+    ind.observations ->> '228cd385-cdf3-46cf-82d5-81a8dc948f86' AS "Activity Description",
+	u.username AS "Added By",
+	al.title AS "Address",
+	ind.registration_location AS "GPS Coordinates"
+FROM
+	individual ind
+	LEFT OUTER JOIN concept c1 ON ind.observations ->> '32d31209-1888-42d4-8d7b-e3770e9e3d6e' = c1.uuid
+JOIN users u ON ind.created_by_id = u.id
+	LEFT OUTER JOIN address_level al ON ind.address_id = al.id
+WHERE
+	ind.subject_type_id = 3 and cast( ind.observations ->> '2d769525-e4b4-4ba7-9253-9d071b83f115'  as date ) BETWEEN '#startDate#' AND '#endDate#';
