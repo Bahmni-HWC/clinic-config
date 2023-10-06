98b5c38e9be9241ace26623b1fe713ee8c63e707
MOHANKUMAR T <mohan13081999@gmail.com>
2023-10-06 17:34:34 +0530

    GOK-389 | Refactor. Registration report to use visit table for location

--- openmrs/apps/reports/sql/registeredPatients.sql
+++ openmrs/apps/reports/sql/registeredPatients.sql
@@ -20,15 +20,15 @@ FROM (
   JOIN (SELECT pri.patient_id, pri.identifier
         FROM patient_identifier pri
         JOIN patient_identifier_type pit ON pri.identifier_type = pit.patient_identifier_type_id AND pit.retired is FALSE AND pri.preferred = 1
-        JOIN global_property gp ON gp.property="bahmni.primaryIdentifierType" AND INSTR (gp.property_value, pit.uuid)) primaryIdentifier 
+        JOIN global_property gp ON gp.property="bahmni.primaryIdentifierType" AND INSTR (gp.property_value, pit.uuid)) primaryIdentifier
     ON pt.patient_id = primaryIdentifier.patient_id
   LEFT OUTER JOIN (SELECT ei.patient_id, ei.identifier
                    FROM patient_identifier ei
                    JOIN patient_identifier_type pit ON ei.identifier_type = pit.patient_identifier_type_id AND pit.retired is FALSE AND ei.preferred = 1
-                   JOIN global_property gp ON gp.property="bahmni.extraPatientIdentifierTypes" AND INSTR (gp.property_value, pit.uuid)) extraIdentifier 
+                   JOIN global_property gp ON gp.property="bahmni.extraPatientIdentifierTypes" AND INSTR (gp.property_value, pit.uuid)) extraIdentifier
     ON pt.patient_id = extraIdentifier.patient_id
-  LEFT JOIN encounter e ON e.patient_id = pt.patient_id 
-  LEFT JOIN location loc ON e.location_id = loc.location_id
+  LEFT JOIN visit v ON v.patient_id = pt.patient_id
+  LEFT JOIN location loc ON v.location_id = loc.location_id
   WHERE pt.voided is FALSE
   AND cast(CONVERT_TZ(pt.date_created,'+00:00','+5:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#'
 GROUP BY pt.patient_id )
