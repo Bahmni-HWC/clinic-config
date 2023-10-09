b180b7c6479cd5934d5a26d20d3052e4585f54fb
MOHANKUMAR T <mohan13081999@gmail.com>
2023-10-09 15:25:50 +0530

    GOK-383 | Refactor e-sanjeevani report to group by esanjeevani username

--- openmrs/apps/reports/sql/esanjeevaniVisitCount.sql
+++ openmrs/apps/reports/sql/esanjeevaniVisitCount.sql
@@ -1,20 +1,16 @@
 SELECT
-    u.username                                                      AS 'User',
-    CONCAT_WS(' ', pn.given_name, pn.middle_name, pn.family_name)   AS 'Name',
-    COUNT(a.event_type)                                             AS 'Number of times E-Sanjeevani Portal was Accessed'
+    SUBSTRING_INDEX(SUBSTRING_INDEX(message, ' ', 2),' ',-1)    AS Username,
+    count(*)                                                    AS 'Number of eSanjeevani Consultations'
 FROM
-    audit_log a
-    INNER JOIN users u ON u.user_id = a.user_id
-    INNER JOIN person p ON p.person_id = u.person_id
-    INNER JOIN person_name pn ON pn.person_id = p.person_id
-    WHERE 
+    audit_log
+    WHERE
         CAST(
                 CONVERT_TZ
                 (
-                    a.date_created, '+00:00', '+5:30'
-                ) 
+                    date_created, '+00:00', '+5:30'
+                )
             AS DATE
-        ) 
+        )
         BETWEEN '#startDate#' AND '#endDate#'
-        AND a.event_type LIKE '%ACCESSED_E_SANJEEVANI%'
-    GROUP BY u.username;
+        AND event_type LIKE '%ACCESSED_E_SANJEEVANI%'
+    GROUP BY Username;
